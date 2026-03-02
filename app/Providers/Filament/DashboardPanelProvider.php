3c0c1ed53cb5b1c9aa42d16e4ee6d8c0bda48105
Walter Prorok <waltprorok@gmail.com>
2026-03-02 15:39:35 -0500

    M3: auth redirect route

--- app/Providers/Filament/DashboardPanelProvider.php
+++ app/Providers/Filament/DashboardPanelProvider.php
@@ -29,7 +29,7 @@ class DashboardPanelProvider extends PanelProvider
             ->path('dashboard')
             ->login()
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => Color::Green,
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
