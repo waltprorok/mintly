a58d012b19bccb40ec55b32e9f30a8b04c4baa21
Walter Prorok <waltprorok@gmail.com>
2026-04-19 12:14:06 -0400

    M48 updated UI support page

--- resources/views/filament/pages/support.blade.php
+++ resources/views/filament/pages/support.blade.php
@@ -1,11 +1,19 @@
 <x-filament::page>
-    <form wire:submit="submit" class="space-y-6">
-        {{ $this->form }}
 
-        <br>
+    <div class="max-w-xl mx-auto">
+        <x-filament::section heading="Contact Support">
+            <form wire:submit="submit" class="space-y-6">
+                {{ $this->form }}
 
-        <x-filament::button type="submit">
-            Send Message
-        </x-filament::button>
-    </form>
+                <br/>
+
+                <x-filament::button type="submit">
+                    Send Message
+                </x-filament::button>
+
+            </form>
+
+        </x-filament::section>
+    </div>
+
 </x-filament::page>
