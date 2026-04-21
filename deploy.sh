4385677027fe9166bf6665260b30f3b695f18620
Walter Prorok <waltprorok@gmail.com>
2026-04-21 17:19:25 -0400

    M50 fixed Prod

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-
 # php artisan down
-
 git pull
 
 export COMPOSER_ALLOW_SUPERUSER=1
@@ -15,14 +13,14 @@ php artisan migrate --force
 npm ci
 npm run build
 
-#php artisan filament:upgrade || true
+php artisan filament:upgrade || true
 
-#php artisan filament:optimize
+php artisan filament:optimize
 #
-#php artisan config:cache
-#php artisan route:cache
-#php artisan view:cache
-#php artisan event:cache
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+php artisan event:cache
 
 php artisan queue:restart
 #php artisan pulse:restart
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
 use App\Http\Controllers\Auth\RegisterController;
-use App\Mail\WelcomeMintlyUser;
-use App\Models\User;
 use Illuminate\Support\Facades\Route;
 use Spatie\Honeypot\ProtectAgainstSpam;
 
@@ -16,21 +14,6 @@ use Spatie\Honeypot\ProtectAgainstSpam;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-/*
- * TODO: Temporary code to test emails LIVE
- */
-Route::get('/test-email', function () {
-    // Grab an existing user (or fake one)
-    $user = User::first();
-    if (! $user) {
-        return 'No users found in database.';
-    }
-    Mail::to($user->email)->send(
-        new WelcomeMintlyUser($user)
-    );
-    return 'Test email sent!';
-});
 
 Route::get('/', function () {
     return view('welcome');
