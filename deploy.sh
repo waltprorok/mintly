2d19fe2478566589f5830ced71bfe63bff637c09
Walter Prorok <waltprorok@gmail.com>
2026-04-21 12:38:49 -0400

    M49 added deploy script

---
+++ deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# php artisan down
+
+git pull
+
+export COMPOSER_ALLOW_SUPERUSER=1
+composer install --prefer-dist --no-dev --optimize-autoloader --no-interaction
+
+php artisan optimize:clear
+php artisan filament:optimize-clear
+
+php artisan migrate --force
+
+npm ci
+npm run build
+
+php artisan filament:upgrade || true
+php artisan filament:optimize
+
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+php artisan event:cache
+
+php artisan queue:restart
+#php artisan pulse:restart
+
+sudo supervisorctl reread
+sudo supervisorctl update
+sudo supervisorctl restart all
+sudo supervisorctl status
+# php artisan up
