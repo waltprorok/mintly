8d9f619a476ca2e0b01aa4a708f377ca10bf9921
Walter Prorok <waltprorok@gmail.com>
2026-04-28 07:59:52 -0400

    M57 color changes to much green

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -81,7 +81,7 @@
 
                             {{-- Submit --}}
                             <div class="d-grid mb-3">
-                                <button type="submit" class="btn btn-success rounded-3">
+                                <button type="submit" class="btn btn-primary rounded-3">
                                     Continue
                                 </button>
                             </div>
