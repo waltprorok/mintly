1382f17953eeafec94707e163e18f336b96afb55
Walter Prorok <waltprorok@gmail.com>
2026-08-13 17:10:48 -0400

    M75 send me an email when a new user signs up

--- app/Notifications/NewUserRegistered.php
+++ app/Notifications/NewUserRegistered.php
@@ -4,10 +4,11 @@ namespace App\Notifications;
 
 use App\Models\User;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class NewUserRegistered extends Notification
+class NewUserRegistered extends Notification implements ShouldQueue
 {
     use Queueable;
 
